520ab2e69a8d6b6188549279796390fb5c5c2f64
Agustin3z <agustin3z@live.com>
2025-05-29 16:31:07 -0300

    logueo

---
+++ main.js
@@ -0,0 +1,33 @@
+const usuario='agustin'
+const contraseña='777'
+ 
+//Bienvenida al programa
+alert('Bienvenido a continuacion necesitaremos su login para entrar al sistema')
+
+// creaciones de variables
+
+let cantidadintentos=3;
+let ingreso=false;
+
+// Logueo
+
+for(i=0; i<cantidadintentos; i++){
+
+    let useringresado= prompt ("Ingresar el usuario")
+    let passingresado= prompt ("Ingresar contraseña")
+
+    if(useringresado == null || passingresado == null){
+        alert("usuario o contraseña vacios")
+    }else {
+        if(usuario === useringresado && contraseña === passingresado){
+        alert("Verificacion confirmada")
+        ingreso = true
+        break
+        
+        } else if (usuario != useringresado){
+            alert("usuario incorrecto")
+        }else if (usuario === useringresado && contraseña != passingresado){
+            alert("contraseña incorrecta")
+        }
+    }
+}
